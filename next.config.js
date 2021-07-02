abbd770a931cac62cc319f8e770c648f418ba6e8
reeq20 <rikusugawara@gmail.com>
2021-07-02 10:13:29 +0900

    fix

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
   },
   exportTrailingSlash: true,
   env: {
-    API_KEY: process.env.API_KEY,
+    API_KEY: process.env.NEXT_PUBLIC_MICRO_CMS_API_KEY,
     SECRET_KEY: process.env.SECRET_KEY,
     END_POINT: process.env.END_POINT,
     BASE_PATH: 'https://riku-sugawara.tech',
